7e1c76c013705f7944528b14c0cf3f2ed5e0e566
Md Shahadat Hossain <shahadat.zcpe@gmail.com>
2020-05-28 21:02:57 +0600

    Modal added

--- resources/views/ddad/devices/create-modal.blade.php
+++ resources/views/ddad/devices/create-modal.blade.php
@@ -1,57 +1,72 @@
 <!--  create model -->
-<form id="devices-create-modal-form" action="{{ route('zones.store') }}" method="post" autocomplete="off">
+<form id="devices-create-modal-form" action="{{ route('detectors.store') }}" method="post" autocomplete="off">
     @csrf
-    <div class="modal fade" id="devices-create-modal" tabindex="-1" role="dialog" aria-labelledby="modal-createLabel" aria-hidden="true">
+    <div class="modal fade" id="devices-create-modal" tabindex="-1" role="dialog" aria-labelledby="modal-createLabel"
+         aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
 
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modal-createLabel">Create new device</h5>
+                    <h5 class="modal-title" id="modal-createLabel">Devices form</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
 
-                    <div class="st_level_up  form-group">
-                        <label>Zone name</label>
-                        <input name="name" required type="text" class="form-control" >
-                        <div class="st_error_message"></div>
-                    </div>
-                    <strong>Locations:</strong><br>
-                    <div class="locations">
-                        <div class="form-group location-field">
-                            <input name="locations[]"  type="text" class="form-control">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="st_level_up form-group">
+                                <label for="name">Android label*</label>
+                                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
+                                       id="name" value="{{ old('name') }}" required>
+                                <div class="st_error_message"></div>
+                            </div>
+                            <div class="st_level_up form-group">
+                                <label for="owner_name">Android IMEI*</label>
+                                <input type="text" name="owner_name"
+                                       class="form-control @error('owner_name') is-invalid @enderror" id="owner_name"
+                                       value="{{ old('owner_name') }}" required>
+                                <div class="st_error_message"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="st_level_up form-group">
+                                <label for="name">Detector label*</label>
+                                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
+                                       id="name" value="{{ old('name') }}" required>
+                                <div class="st_error_message"></div>
+                            </div>
+                            <div class="st_level_up form-group">
+                                <label for="owner_name">Serial IMEI*</label>
+                                <input type="text" name="owner_name"
+                                       class="form-control @error('owner_name') is-invalid @enderror" id="owner_name"
+                                       value="{{ old('owner_name') }}" required>
+                                <div class="st_error_message"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="st_level_up form-group">
+                                <label for="name">TV label*</label>
+                                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
+                                       id="name" value="{{ old('name') }}" required>
+                                <div class="st_error_message"></div>
+                            </div>
+                            <div class="st_level_up form-group">
+                                <label for="owner_name">TV serial*</label>
+                                <input type="text" name="owner_name"
+                                       class="form-control @error('owner_name') is-invalid @enderror" id="owner_name"
+                                       value="{{ old('owner_name') }}" required>
+                                <div class="st_error_message"></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-light" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Save</button>
-                </div>
-            </div><!-- /.modal-content -->
+                    <button type="submit" class="btn btn-primary">Save device</button>
 
 
-        </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
+
+                </div>
+            </div>
+        </div>
+    </div>
 </form>
-
-
-@push('script')
-    <script type="text/javascript">
-        // this is the id of the form
-        $(document).on('keyup', '.location-field input', function() {
-            $('.location-field input').each(function(index) {
-                var isBlank = !$(this).val();
-                if(isBlank) {
-                    $(this).remove()
-                }
-            });
-
-            $('.locations').append($('<div class="form-group location-field">\n' +
-                '                            <input name="locations[]"  type="text" class="form-control">\n' +
-                '                        </div>'));
-
-        });
-    </script>
-@endpush
