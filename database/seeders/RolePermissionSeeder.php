427b3e51c0956856349b387c626f6ac6a18b2e29
asifhaider995 <asif.haider995@gmail.com>
2020-09-27 00:11:04 +0600

    seeders and factories

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
--- database/seeders/SettingsTableSeeder.php
+++ database/seeders/SettingsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Database\Seeders;
 use App\Models\Setting;
 use Illuminate\Database\Seeder;
 
